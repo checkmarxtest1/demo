6624ce3d46586a1656d1bbd661d08986f8a53908
PeterT <petert@checkmarx.com>
2015-05-17 16:29:54 +0300

    commit1

---
+++ test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    public class test
+    {
+       public void  XSS()
+       {
+            Response.Write(Request.Form["data"]);
+       }
+
+       public void Privacy()
+       {
+           string password = "123456";
+       }
+
+       public void SQLi()
+       {
+           using (SqlConnection c = new SqlConnection())
+           {
+
+               SqlCommand cmd = new SqlCommand("SELECT * FROM Table WHERE " + Request.Form["data"]);
+               cmd.ExecuteNonQuery();
+           
+           }
+       }
+
+
+
+       
+    }
+}
